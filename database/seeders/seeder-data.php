7c353e97dc00fcb5ea6bb3199cea3e36a8016eba
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-12 18:10:49 +0200

    feat: seeder array generated

---
+++ database/seeders/seeder-data.php
@@ -0,0 +1,244 @@
+<?php
+return [
+    [
+        'title' => 'Appartamento con vista sul mare',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 75,
+        'available' => true
+    ],
+    [
+        'title' => 'Monolocale in centro città',
+        'rooms' => 1,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 40,
+        'available' => false
+    ],
+    [
+        'title' => 'Bilocale in periferia',
+        'rooms' => 2,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 50,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento moderno con terrazza',
+        'rooms' => 4,
+        'beds' => 3,
+        'bathroom' => 2,
+        'square_mt' => 100,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento storico',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 85,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento con giardino',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 90,
+        'available' => true
+    ],
+    [
+        'title' => 'Attico panoramico',
+        'rooms' => 4,
+        'beds' => 3,
+        'bathroom' => 2,
+        'square_mt' => 120,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento vicino alla stazione',
+        'rooms' => 2,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 55,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento arredato',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 70,
+        'available' => true
+    ],
+    [
+        'title' => 'Monolocale economico',
+        'rooms' => 1,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 35,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento con piscina',
+        'rooms' => 5,
+        'beds' => 4,
+        'bathroom' => 3,
+        'square_mt' => 150,
+        'available' => true
+    ],
+    [
+        'title' => 'Bilocale arredato',
+        'rooms' => 2,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 45,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento per famiglie',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 2,
+        'square_mt' => 85,
+        'available' => true
+    ],
+    [
+        'title' => 'Attico di lusso',
+        'rooms' => 5,
+        'beds' => 3,
+        'bathroom' => 3,
+        'square_mt' => 140,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento in zona tranquilla',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 75,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento ristrutturato',
+        'rooms' => 2,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 50,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento con balcone',
+        'rooms' => 4,
+        'beds' => 3,
+        'bathroom' => 2,
+        'square_mt' => 95,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento per studenti',
+        'rooms' => 3,
+        'beds' => 3,
+        'bathroom' => 1,
+        'square_mt' => 70,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento con garage',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 80,
+        'available' => true
+    ],
+    [
+        'title' => 'Monolocale di design',
+        'rooms' => 1,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 30,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento con vista montagna',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 75,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento spazioso',
+        'rooms' => 4,
+        'beds' => 3,
+        'bathroom' => 2,
+        'square_mt' => 110,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento accogliente',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 70,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento con doppio soggiorno',
+        'rooms' => 4,
+        'beds' => 2,
+        'bathroom' => 2,
+        'square_mt' => 100,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento moderno in centro',
+        'rooms' => 2,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 60,
+        'available' => true
+    ],
+    [
+        'title' => 'Bilocale economico',
+        'rooms' => 2,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 45,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento con sala fitness',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 2,
+        'square_mt' => 85,
+        'available' => true
+    ],
+    [
+        'title' => 'Appartamento vicino al mare',
+        'rooms' => 4,
+        'beds' => 3,
+        'bathroom' => 2,
+        'square_mt' => 100,
+        'available' => true
+    ],
+    [
+        'title' => 'Monolocale con giardino',
+        'rooms' => 1,
+        'beds' => 1,
+        'bathroom' => 1,
+        'square_mt' => 35,
+        'available' => false
+    ],
+    [
+        'title' => 'Appartamento in zona universitaria',
+        'rooms' => 3,
+        'beds' => 2,
+        'bathroom' => 1,
+        'square_mt' => 65,
+        'available' => true
+    ]
+];
+?>
