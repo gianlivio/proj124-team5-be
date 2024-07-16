d291bb8b9359fb6d707517c47c1153af74bbd65f
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-16 12:03:17 +0200

    feat: mostrati nell'index solo gli appartamenti dell'utente che li ha creati

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -9,28 +9,7 @@ use App\Models\Apartment;
 
 class ApartmentController extends Controller
 {
-    // public function getAddressFromCoordinates($slug){
-    //     $apartment = Apartment::where('slug',$slug)->first();
-
-    //     if (!$apartment) {
-    //         return response()->json(['error' => 'Appartamento non trovato'], 404);
-    //     }
-
-    //     $lat = $apartment->latitude;
-    //     $lon = $apartment->longitude;
-    //     $apiKey = env('TOMTOM_API_KEY');
-
-    //     $url = "https://api.tomtom.com/search/2/reverseGeocode/{$lat},{$lon}.json?key={$apiKey}";
-
-    //     $client = new Client();
-
-    //     $response = $client->get($url);
-    //     $data = json_decode($response->getBody(), true);
-
-    //     $address = $data['addresses'][0]['address']['freeformAddress'];
-
-    //     return response()->json(['address' => $address]);
-    // }
+    
 
     public function getAddressFromCoordinates($slug)
     {
@@ -50,7 +29,7 @@ class ApartmentController extends Controller
 
 
         $response = Http::withOptions([
-            'verify' => false, // Disabilita la verifica del certificato SSL
+            'verify' => false, 
         ])
         ->get($url);
 
@@ -65,34 +44,7 @@ class ApartmentController extends Controller
     }
 
 
-    // public function getCoordinatesForAddress($address)
-    // {
-    //     $apiKey = env('TOMTOM_API_KEY');
-
-    //     $client = new Client();
-
-    //     // Codifica l'indirizzo per essere utilizzato in un URL
-    //     $encodedAddress = urlencode($address);
-
-    //     // Url per la richiesta
-    //     $url = "https://api.tomtom.com/search/2/geocode/{$encodedAddress}.json?key={$apiKey}";
-
-    //     try {
-    //         // Effettua la richiesta GET all'API di TomTom
-    //         $response = $client->get($url);
-
-    //         // Decodifica la risposta JSON
-    //         $data = json_decode($response->getBody(), true);
-
-    //         // Estrai le coordinate dalla risposta
-    //         $latitude = $data['results'][0]['position']['lat'];
-    //         $longitude = $data['results'][0]['position']['lon'];
-
-    //         return ['latitude' => $latitude, 'longitude' => $longitude];
-    //     } catch (\Exception $e) {
-    //         return null;
-    //     }
-    // }
+    
 
     public function getCoordinatesForAddress($address)
     {
@@ -106,7 +58,7 @@ class ApartmentController extends Controller
             
             // $response = Http::get($url);
             $response = Http::withOptions([
-                'verify' => false, // Disabilita la verifica del certificato SSL
+                'verify' => false, 
             ])
             ->get($url);
 
