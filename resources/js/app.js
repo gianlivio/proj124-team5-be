93dd3f9a69addf68f6d0bc68531a2ca1c2c11004
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-16 12:24:17 +0200

    feat: modal for delete in index added

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,20 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 import * as bootstrap from "bootstrap";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Mostra il modale e imposta l'action da eliminare
+    $('#confirmModal').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var action = button.data('action'); // Extract info from data-* attributes
+        var modal = $(this);
+        modal.find('#deleteButton').attr('href', action);
+    });
+
+    // Gestisce l'eliminazione
+    $('#confirmModal').on('click', '#deleteButton', function(e) {
+        e.preventDefault();
+        window.location.href = $(this).attr('href');
+    });
+});
--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -5,18 +5,18 @@
         <div class="ms-table-container mt-5">
             <div class="d-flex justify-content-between align-items-center">
                 <h1 class="fw-bold">Appartamenti</h1>
-                
+
                 <div class="d-flex flex-column">
-                    <a href="{{route('admin.apartments.create')}}" class="btn btn-primary fw-bold">Add New </a>
-            <span class="fw-bold">Total row: <?= count($apartments)?> </span>
+                    <a href="{{ route('admin.apartments.create') }}" class="btn btn-primary fw-bold">Add New </a>
+                    <span class="fw-bold">Total row: <?= count($apartments) ?> </span>
 
                 </div>
-                
+
             </div>
             <hr>
-            
-                <table class="table table-striped">
-                    <thead>
+
+            <table class="table table-striped">
+                <thead>
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Titolo</th>
@@ -26,35 +26,62 @@
                         <th scope="col">Metri Quadrati</th>
                         <th scope="col">Disponibile</th>
                     </tr>
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
 
-                        @foreach ($apartments as $curApartment)
-                            <tr>
-                                <th scope="row">{{ $loop->index + 1}}</th>
-                                <td>{{ $curApartment->title }}</td>
-                                <td>{{ $curApartment->rooms }}</td>
-                                <td>{{ $curApartment->beds }}</td>
-                                <td>{{ $curApartment->bathrooms }}</td>
-                                <td>{{ $curApartment->square_mt }}</td>
-                                <td>{{ $curApartment->available ? 'si' : 'no' }}</td>
-                                <td>
-                                    <div class="d-flex gap-2">
-                                        <a href="{{ route('admin.apartments.show',['apartment'=>$curApartment->slug])  }}" class="btn btn-success fw-bold text-light">Dettagli</a>
-                                        <a href="{{ route('admin.apartments.edit',['apartment'=>$curApartment->slug])  }}" class="btn btn-warning fw-bold text-light">Modifica</a>
-                                        <form action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn fw-bold btn-danger" onclick="return confirm('Sei sicuro di voler procedere?')">Elimina</button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
+                    @foreach ($apartments as $curApartment)
+                        <tr>
+                            <th scope="row">{{ $loop->index + 1 }}</th>
+                            <td>{{ $curApartment->title }}</td>
+                            <td>{{ $curApartment->rooms }}</td>
+                            <td>{{ $curApartment->beds }}</td>
+                            <td>{{ $curApartment->bathrooms }}</td>
+                            <td>{{ $curApartment->square_mt }}</td>
+                            <td>{{ $curApartment->available ? 'si' : 'no' }}</td>
+                            
+                            <td>
+                                <div class="d-flex gap-2">
+                                    <a href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}"
+                                        class="btn btn-success fw-bold text-light">Dettagli</a>
+                                    <a href="{{ route('admin.apartments.edit', ['apartment' => $curApartment->slug]) }}"
+                                        class="btn btn-warning fw-bold text-light">Modifica</a>
+                                    <form
+                                        action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" class="btn fw-bold btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal" data-action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}">Elimina</button>
+                                        <div class="modal fade" id="confirmModal" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Conferma Eliminazione
+                                                        </h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Sei sicuro di voler eliminare questo appartamento?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Annulla</button>
+                                                        <button type="submit" class="btn btn-danger"
+                                                            id="deleteButton">Elimina</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
 
-                        
-                    </tbody>
-                </table>
+
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
