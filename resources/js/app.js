a46e6c1038f0c476942cde7f7cb7b02cc4c1eb9d
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-17 16:09:03 +0200

    feat:minor change to autocomplete function

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,19 @@ import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 import * as bootstrap from "bootstrap";
 
-
 document.addEventListener('DOMContentLoaded', function() {
-
     const addressInput = document.getElementById('address');
     const suggestionsContainer = document.getElementById('suggestions');
+    let timeout;
 
     addressInput.addEventListener('input', function() {
         let query = this.value;
+
+        // Clear previous timeout to prevent multiple timeouts
+        if (timeout) clearTimeout(timeout);
+
         if (query.length >= 4) {
-            timeout = setTimeout(function(){
-                
+            timeout = setTimeout(function() {
                 axios.get('/api/autocomplete', {
                     params: {
                         query: query
@@ -27,24 +29,25 @@ document.addEventListener('DOMContentLoaded', function() {
                         suggestion.classList.add('list-group-item', 'list-group-item-action', 'suggestion-item');
                         suggestion.textContent = item.address;
                         suggestionsContainer.appendChild(suggestion);
-    
-                        suggestion.addEventListener('click', function() {
+
+                        suggestion.addEventListener('click', function(event) {
+                            event.preventDefault();
                             addressInput.value = item.address;
                             suggestionsContainer.innerHTML = '';
-                            clearInterval(timeout);
                         });
                     });
                 })
                 .catch(function(error) {
-                    console.error('Errore durante la ricerca:', error);
+                    console.error('Error during search:', error);
+                    // Optionally display an error message to the user
                 });
-            }, 500);
+            }, 500); // Debounce time in milliseconds
         } else {
             suggestionsContainer.innerHTML = '';
         }
     });
-    
-    // Mostra il modale e imposta l'action da eliminare
+
+    // Show modal and set the action to be deleted
     $('#confirmModal').on('show.bs.modal', function(event) {
         var button = $(event.relatedTarget); 
         var action = button.data('action');
@@ -52,10 +55,9 @@ document.addEventListener('DOMContentLoaded', function() {
         modal.find('#deleteButton').attr('href', action);
     });
 
-    // Gestisce l'eliminazione
+    // Handle deletion
     $('#confirmModal').on('click', '#deleteButton', function(e) {
         e.preventDefault();
         window.location.href = $(this).attr('href');
     });
-    
 });
