ea3e632cc036cc656be1f5b328bfa2fcb385874c
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-17 12:48:45 +0200

    feat: register form now client side working

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form class="needs-validation" method="POST" action="{{ route('register') }}">
+                    <form id="registerForm" class="needs-validation" novalidate method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="mb-4 row">
@@ -17,6 +17,9 @@
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
+                                <div class="invalid-feedback">
+                                    Perfavore inserisci il nome.
+                                </div>
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -31,6 +34,9 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
+                                <div class="invalid-feedback">
+                                    Perfavore inserisci l'email.
+                                </div>
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -45,8 +51,11 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password"  class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" minlength="8" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
+                                <div id="password_fb" class="invalid-feedback">
+                                    Perfavore inserisci una password.
+                                </div>
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -60,6 +69,8 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <div class="invalid-feedback">
+                                </div>
                             </div>
                         </div>
 
@@ -78,8 +89,33 @@
 </div>
 
 
-<script> 
-    
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    (function () {
+            'use strict';
+            window.addEventListener('load', function () {
+                let form = document.getElementById('registerForm');
+                form.addEventListener('submit', function (event) {
+                    let passwordInput = document.getElementById('password');
+                    if (passwordInput.value.length < 8 && passwordInput.value.length > 0) {
+                        let feedback_length = document.getElementById("password_fb")
+                        console.log(feedback_length.innerHTML);
+                        feedback_length.innerHTML = "La password deve essere di almeno 8 caratteri."
+                        
+                        passwordInput.setCustomValidity('La password deve essere di almeno 8 caratteri.');
+                    } else {
+                        passwordInput.setCustomValidity('');
+                    }
+
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+
+                    form.classList.add('was-validated');
+                }, false);
+            }, false);
+        })();
 </script>
 
 @endsection
