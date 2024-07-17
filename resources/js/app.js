3da3d23667639487f96048a0059deca7371bfb5f
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-17 15:35:10 +0200

    feat: some changes in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,59 +3,64 @@ import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 import * as bootstrap from "bootstrap";
 
+document.addEventListener("DOMContentLoaded", function () {
+    const addressInput = document.getElementById("address");
+    const suggestionsContainer = document.getElementById("suggestions");
+    let lastQuery = "";
 
-document.addEventListener('DOMContentLoaded', function() {
-
-    const addressInput = document.getElementById('address');
-    const suggestionsContainer = document.getElementById('suggestions');
-
-    addressInput.addEventListener('input', function() {
+    addressInput.addEventListener("input", function () {
         let query = this.value;
-        if (query.length >= 4) {
-            timeout = setTimeout(function(){
-                
-                axios.get('/api/autocomplete', {
-                    params: {
-                        query: query
-                    }
-                })
-                .then(function(response) {
-                    suggestionsContainer.innerHTML = '';
-                    response.data.forEach(function(item) {
-                        let suggestion = document.createElement('a');
-                        suggestion.href = '#';
-                        suggestion.classList.add('list-group-item', 'list-group-item-action', 'suggestion-item');
-                        suggestion.textContent = item.address;
-                        suggestionsContainer.appendChild(suggestion);
-    
-                        suggestion.addEventListener('click', function() {
-                            addressInput.value = item.address;
-                            suggestionsContainer.innerHTML = '';
-                            clearInterval(timeout);
+        console.log(`Query: ${query}, Last Query: ${lastQuery}`);
+        if (query !== lastQuery && query.length >= 4) {
+            
+            let timeout = setTimeout(function () {
+                axios
+                    .get("/api/autocomplete", {
+                        params: {
+                            query: query,
+                        },
+                    })
+                    .then(function (response) {
+                        suggestionsContainer.innerHTML = "";
+                        response.data.forEach(function (item) {
+                            let suggestion = document.createElement("a");
+                            suggestion.href = "#";
+                            suggestion.classList.add(
+                                "list-group-item",
+                                "list-group-item-action",
+                                "suggestion-item"
+                            );
+                            suggestion.textContent = item.address;
+                            suggestionsContainer.appendChild(suggestion);
+
+                            suggestion.addEventListener("click", function () {
+                                addressInput.value = item.address;
+                                suggestionsContainer.innerHTML = "";
+                                clearInterval(timeout);
+                            });
                         });
+                    })
+                    .catch(function (error) {
+                        console.error("Errore durante la ricerca:", error);
                     });
-                })
-                .catch(function(error) {
-                    console.error('Errore durante la ricerca:', error);
-                });
+                lastQuery = query;
             }, 500);
         } else {
-            suggestionsContainer.innerHTML = '';
+            suggestionsContainer.innerHTML = "";
         }
     });
-    
+
     // Mostra il modale e imposta l'action da eliminare
-    $('#confirmModal').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget); 
-        var action = button.data('action');
+    $("#confirmModal").on("show.bs.modal", function (event) {
+        var button = $(event.relatedTarget);
+        var action = button.data("action");
         var modal = $(this);
-        modal.find('#deleteButton').attr('href', action);
+        modal.find("#deleteButton").attr("href", action);
     });
 
     // Gestisce l'eliminazione
-    $('#confirmModal').on('click', '#deleteButton', function(e) {
+    $("#confirmModal").on("click", "#deleteButton", function (e) {
         e.preventDefault();
-        window.location.href = $(this).attr('href');
+        window.location.href = $(this).attr("href");
     });
-    
 });
