53e0f6975db4ab931ea23b477b9ab3371db906df
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-18 10:16:03 +0200

    fix Illuminate missing in apartment

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
