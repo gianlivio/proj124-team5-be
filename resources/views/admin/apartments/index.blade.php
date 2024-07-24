54deafd0c21be874e80b95ba4872ccd14aa6fb01
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-07-24 11:51:03 +0200

    feat:resp dashboard and index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
     <div class="container mb-4">
-
-
         @if (session('message'))
             <div class="alert alert-success mt-2 bg-white text-orange">
                 {{ session('message') }}
@@ -16,135 +14,91 @@
             </div>
         @endif
 
-        
-        
         <div class="ms-table-container mt-5">
-            <div class="d-flex justify-content-between align-items-center">
-                <h1 class="fw-bold text-white">Appartamenti</h1>
-                <div class="d-flex flex-column">
-                    <a href="{{ route('admin.apartments.create') }}" class="btn btn-orange fw-bold">Aggiungi</a>
+            <div class="d-flex justify-content-between align-items-center flex-column flex-md-row">
+                <h1 class="fw-bold text-white mb-3 mb-md-0">Appartamenti</h1>
+                <div class="d-flex flex-column align-items-center">
+                    <a href="{{ route('admin.apartments.create') }}" class="btn btn-orange fw-bold mb-2 mb-md-0">Aggiungi</a>
                     <span class="fw-bold text-white">Attuali: {{ count($apartments) }}</span>
                 </div>
             </div>
 
             <div class="bg-white p-4 rounded-3 mt-2" id="apartment_list_index">
-                <table class="w-100">
-                    <thead class="fw-bold">
-                        <tr>
-                            <th class="ps-3 rounded-start-3 ">Titolo</th>
-                            <th>stanze</th>
-                            <th>letti</th>
-                            <th>bagni</th>
-                            <th>mq</th>
-                            <th>stato</th>
-                            <th class="pe-3 text-end rounded-end-3">azioni</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($apartments as $curApartment)
+                <div class="table-responsive"> <!-- Aggiungere questa classe per rendere la tabella scrollabile su dispositivi mobili -->
+                    <table class="table">
+                        <thead class="fw-bold">
                             <tr>
-                                <td class="pl-3">
-                                    <div class="d-flex align-items-center">
-
-                                        <a
-                                            href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">
-                                            @if ($curApartment->img_path)
-                                                <img class="img-fluid"
-                                                    src="{{ asset('storage/' . $curApartment->img_path) }}"
-                                                    alt="{{ $curApartment->title }}">
-                                            @else
-                                                <img class="img-fluid"
-                                                    src="https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png"
-                                                    alt="{{ $curApartment->title }}">
-                                            @endif
-                                        </a>
-                                        <div class="ps-4">
-                                            <p class="fw-medium m-0">
-                                                <a class=""
-                                                    href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">{{ $curApartment->title }}</a>
-                                            </p>
-                                            <p class="muted m-0">Address</p>
-                                            {{-- <p class="fw-bold m-0">125€</p> --}}
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>{{ $curApartment->rooms }}</td>
-                                <td>{{ $curApartment->beds }}</td>
-                                <td>{{ $curApartment->bathroom }}</td>
-                                <td>{{ $curApartment->square_mt }}</td>
-                                <td>
-                                    {!! $curApartment->available
-                                        ? '<p class="my_chips active m-0">Si</p>'
-                                        : '<p class="my_chips deactive m-0">No</p>' !!}
-                                </td>
-                                <td class="text-end px-3">
-                                    <div class="dropdown">
-                                        <a class="ellipsis-menu" type="button" data-bs-toggle="dropdown"
-                                            aria-expanded="false">
-                                            <i class="fa-solid fa-ellipsis-vertical fs-4"></i>
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li>
-                                                <a class="dropdown-item" type="button"
-                                                    href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">Dettagli</a>
-                                            </li>
-                                            <li><a class="dropdown-item"
-                                                    href="{{ route('admin.apartments.edit', ['apartment' => $curApartment->slug]) }}"
-                                                    type="button">Modifica</a></li>
-                                            <li>
-                                                <button type="button" class="dropdown-item" data-bs-toggle="modal"
-                                                    data-bs-target="#confirmModal"
-                                                    data-action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}">Elimina</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </td>
+                                <th class="ps-3 rounded-start-3">Titolo</th>
+                                <th>stanze</th>
+                                <th>letti</th>
+                                <th>bagni</th>
+                                <th>mq</th>
+                                <th>stato</th>
+                                <th class="pe-3 text-end rounded-end-3">azioni</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($apartments as $curApartment)
+                                <tr>
+                                    <td class="pl-3">
+                                        <div class="d-flex align-items-center">
+                                            <a href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">
+                                                @if ($curApartment->img_path)
+                                                    <img class="img-fluid"
+                                                        src="{{ asset('storage/' . $curApartment->img_path) }}"
+                                                        alt="{{ $curApartment->title }}">
+                                                @else
+                                                    <img class="img-fluid"
+                                                        src="https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png"
+                                                        alt="{{ $curApartment->title }}">
+                                                @endif
+                                            </a>
+                                            <div class="ps-4">
+                                                <p class="fw-medium m-0">
+                                                    <a class=""
+                                                        href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">{{ $curApartment->title }}</a>
+                                                </p>
+                                                <p class="muted m-0">Address</p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>{{ $curApartment->rooms }}</td>
+                                    <td>{{ $curApartment->beds }}</td>
+                                    <td>{{ $curApartment->bathroom }}</td>
+                                    <td>{{ $curApartment->square_mt }}</td>
+                                    <td>
+                                        {!! $curApartment->available
+                                            ? '<p class="my_chips active m-0">Si</p>'
+                                            : '<p class="my_chips deactive m-0">No</p>' !!}
+                                    </td>
+                                    <td class="text-end px-3">
+                                        <div class="dropdown">
+                                            <a class="ellipsis-menu" type="button" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <i class="fa-solid fa-ellipsis-vertical fs-4"></i>
+                                            </a>
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a class="dropdown-item" type="button"
+                                                        href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}">Dettagli</a>
+                                                </li>
+                                                <li><a class="dropdown-item"
+                                                        href="{{ route('admin.apartments.edit', ['apartment' => $curApartment->slug]) }}"
+                                                        type="button">Modifica</a></li>
+                                                <li>
+                                                    <button type="button" class="dropdown-item" data-bs-toggle="modal"
+                                                        data-bs-target="#confirmModal"
+                                                        data-action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}">Elimina</button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-            {{-- <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Titolo</th>
-                        <th scope="col">Stanze</th>
-                        <th scope="col">Letti</th>
-                        <th scope="col">Bagni</th>
-                        <th scope="col">Metri Quadrati</th>
-                        <th scope="col">Disponibile</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($apartments as $curApartment)
-                        <tr>
-                            <th scope="row">{{ $loop->index + 1 }}</th>
-                            <td>{{ $curApartment->title }}</td>
-                            <td>{{ $curApartment->rooms }}</td>
-                            <td>{{ $curApartment->beds }}</td>
-                            <td>{{ $curApartment->bathroom }}</td>
-                            <td>{{ $curApartment->square_mt }}</td>
-                            <td>{{ $curApartment->available ? 'si' : 'no' }}</td>
-                            <td>
-                                <div class="d-flex gap-2">
-                                    <a href="{{ route('admin.apartments.show', ['apartment' => $curApartment->slug]) }}"
-                                        class="btn btn-success fw-bold text-light">Dettagli</a>
-                                    <a href="{{ route('admin.apartments.edit', ['apartment' => $curApartment->slug]) }}"
-                                        class="btn btn-primary fw-bold text-light">Modifica</a>
-                                    <button type="button" class="btn fw-bold btn-danger" data-bs-toggle="modal"
-                                        data-bs-target="#confirmModal"
-                                        data-action="{{ route('admin.apartments.destroy', ['apartment' => $curApartment->slug]) }}">Elimina</button>
-                                    <a href="{{ route('admin.apartments.list_sponsor', ['apartment' => $curApartment->slug]) }}"
-                                        class="btn btn-warning fw-bold text-light"><i class="fa-solid fa-crown"></i></a>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table> --}}
         </div>
     </div>
 
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,11 +8,9 @@
     </div>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 mt-4">
+            <div class="col-12 col-md-8 mt-4">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
-
-
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
