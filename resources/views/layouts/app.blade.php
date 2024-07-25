b97edfb579a5f54a61bf12e3b96c21d4ea9fe12b
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-25 17:29:41 +0200

    fix: double footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,60 +85,8 @@
             @yield('content')
         </main>
 
-        <div class="container_ft">
-            <div class="container">
-                <footer class="fsSmall footer mt-auto">
-                    <div class="links d-flex justify-content-between border-bottom">
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">Termini</a></li>
-                            <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">Privacy</a></li>
-                            <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">FAQs</a></li>
-                            <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">About</a></li>
-                        </ul>
-            
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item">
-                                <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-instagram"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-facebook"></i></a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-x-twitter"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                    <p class="text-center py-2 ">© 2024 BoolBnB, Inc</p>
-                </footer>
-            </div>
-        </div>
+        
     </div>
-
-    <style>
-            .container_ft {
-                background-color: #7EBC89;
-                min-width: 100%;
-                footer {
-                    padding: 5px;
-                    color: #FFFDFD !important;
-                    p {
-                        margin: 0;
-                    }
-        
-                    a {
-                        color: inherit
-                    }
-
-                    a:hover {
-                        color: #FE5D26
-                    }
-                }
-            }
-        
-            .fsSmall {
-                font-size: 0.8rem !important;
-            }
-        </style>
 
     
 </body>
