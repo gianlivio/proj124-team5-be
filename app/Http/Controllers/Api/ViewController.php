3ae8bbf7c4faa369b8bc99862de9e7891de10a38
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-25 09:57:10 +0200

    feat: view register done

---
+++ app/Http/Controllers/Api/ViewController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\View;
+use Illuminate\Http\Request;
+
+class ViewController extends Controller
+{
+    public function store(Request $request) {
+        $view = new View();
+        $view->user_ip = $request->ip();
+        $view->apartment_id = $request->apartment_id;
+        $view->save();
+
+        return response()->json(['result' => 'visita registrata'], 201);
+    }
+}
