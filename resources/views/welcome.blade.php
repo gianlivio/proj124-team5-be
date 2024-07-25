8d633b4b45e1982f7bca370120052ec5843905a5
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-25 17:24:55 +0200

    add: added footer to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,5 +12,30 @@
 
         <p class="col-md-8 fs-4">La Dashboard che ti permette di gestire i tuoi appartamenti in giro per il mondo.</p>
     </div>
+    <div class="container-nav fixed-bottom">
+        <footer class="fsSmall footer mt-auto">
+            <div class="links d-flex justify-content-between border-bottom">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">Termini</a></li>
+                    <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">Privacy</a></li>
+                    <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">FAQs</a></li>
+                    <li class="nav-item"><a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2">About</a></li>
+                </ul>
+    
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-instagram"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://youtu.be/dQw4w9WgXcQ?si=EfCJJohZbHcWNRAC" class="nav-link px-2"><i class="fa-brands fa-x-twitter"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <p class="text-center py-2 ">© 2024 BoolBnB, Inc</p>
+        </footer>
+    </div>
 </div>
 @endsection
