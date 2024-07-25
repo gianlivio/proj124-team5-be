5e4f6071f458eff3259412a6e3776ef79d9d8c98
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-25 14:54:36 +0200

    feat: chart lead added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use App\Models\Lead;
 use App\Models\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +15,29 @@ class DashboardController extends Controller
 public function index()
 {
     $user = Auth::user();
+
     $totalViews = View::where('user_id', $user->id)->count();
-    $viewsCountByApartment = View::select('apartment_id','title', DB::raw('count(*) as total_views'))
-        ->groupBy('apartment_id','title')
-        ->where('user_id', $user->id)
-        ->get();
-    // dd($viewsCountByApartment);
-    return view('admin.dashboard', ['viewsCountByApartment' => $viewsCountByApartment]);
+    // $viewsCountByApartment = View::select('apartment_id','title', DB::raw('count(*) as total_views'))
+    //     ->groupBy('apartment_id','title')
+    //     ->where('user_id', $user->id)
+    //     ->get();
+
+    $apartmentLeads = Lead::select('apartment_id', DB::raw('count(*) as generated_leads'), 'apartments.title')
+    ->leftJoin('apartments', 'leads.apartment_id', '=', 'apartments.id') // Join con la tabella Apartment
+    ->groupBy('leads.apartment_id', 'apartments.title')
+    ->where('apartments.user_id', $user->id)
+    ->get();
+
+
+    $viewsCountByApartment = View::select('apartment_id', DB::raw('count(*) as total_views'), 'apartments.title')
+    ->leftJoin('apartments', 'views.apartment_id', '=', 'apartments.id') // Join con la tabella Apartment
+    ->groupBy('views.apartment_id', 'apartments.title')
+    ->where('views.user_id', $user->id)
+    ->get();
+
+    return view('admin.dashboard', 
+    ['viewsCountByApartment' => $viewsCountByApartment], 
+    ['apartmentLeads' => $apartmentLeads]);
 }
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,53 +21,88 @@
                     </div>
                 </div>
             </div>
-{{-- @dd($viewsCountByApartment) --}}
-            {{-- CHART CONTAINER --}}
-            {{-- @if (!empty($viewsCountByApartment))    
-            <div class="col-12 mt-5 chart mb-5">
-                <h2 class="text-center p-3">statistiche</h2>
-                <canvas id="myChart"></canvas>
-            </div>
-            @endif --}}
 
             @if ($viewsCountByApartment->isNotEmpty())
-            <div class="col-12 mt-5 chart mb-5">
-                <h2 class="text-center p-3">statistiche</h2>
-                <canvas id="myChart"></canvas>
-            </div>
+                <div class="row justify-content-between">
+                    <div class="col-5 mt-5 chart mb-5">
+                        <h2 class="text-center p-3">Visite agli appartamenti</h2>
+                        <canvas id="viewsChart"></canvas>
+                    </div>
+                    <div class="col-5 mt-5 chart mb-5">
+                        <h2 class="text-center p-3">Contatti per appartamento</h2>
+                        <canvas id="leadsChart"></canvas>
+                    </div>
+                </div>
             @else
-            <div class="col-12 mt-5 chart mb-5">
-                <h2 class="text-center p-3">nessuna statistica</h2>
-            </div>
-                
+                <div class="col-12 mt-5 chart mb-5">
+                    <h2 class="text-center p-3">Nessuna statistica disponibile</h2>
+                </div>
             @endif
 
-
-
-            {{-- CHART SCRIPT --}}
             <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
             <script>
-                const labels = @json($viewsCountByApartment->pluck('title'));
-                const data = @json($viewsCountByApartment->pluck('total_views'));
+                const viewLabels = @json($viewsCountByApartment->pluck('title'));
+                const viewData = @json($viewsCountByApartment->pluck('total_views'));
 
-                const ctx = document.getElementById('myChart');
+                const leadLabels = @json($apartmentLeads->pluck('title'));
+                const leadData = @json($apartmentLeads->pluck('generated_leads'));
 
-                new Chart(ctx, {
+                const viewCtx = document.getElementById('viewsChart').getContext('2d');
+                const leadCtx = document.getElementById('leadsChart').getContext('2d');
+
+                new Chart(viewCtx, {
                     type: 'bar',
                     data: {
-                        labels: labels,
+                        labels: viewLabels,
                         datasets: [{
-                            label: 'Andamento visite ai tuoi appartamenti',
-                            data: data,
+                            label: 'Visite agli appartamenti',
+                            data: viewData,
                             backgroundColor: [
-                                'rgba(255, 99, 132, 0.6)',
-                                'rgba(255, 159, 64, 0.6)',
-                                'rgba(255, 205, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(201, 203, 207, 0.6)'
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(255, 205, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(201, 203, 207, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgb(255, 99, 132)',
+                                'rgb(255, 159, 64)',
+                                'rgb(255, 205, 86)',
+                                'rgb(75, 192, 192)',
+                                'rgb(54, 162, 235)',
+                                'rgb(153, 102, 255)',
+                                'rgb(201, 203, 207)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+
+                new Chart(leadCtx, {
+                    type: 'polarArea',
+                    data: {
+                        labels: leadLabels,
+                        datasets: [{
+                            label: 'Contatti per appartamento',
+                            data: leadData,
+                            backgroundColor: [
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(201, 203, 207, 0.2)',
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(255, 159, 64, 0.2)',
+                                'rgba(255, 205, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
                             ],
                             borderColor: [
                                 'rgb(255, 99, 132)',
