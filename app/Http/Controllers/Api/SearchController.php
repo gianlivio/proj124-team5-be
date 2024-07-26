ee4ffecd11102fffffd800c1d47036ccaed37c96
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-26 16:18:16 +0200

    feat: selected sponsorship_id

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -35,7 +35,7 @@ class SearchController extends Controller
                 // checkare se available
                 $locations = DB::table('apartments')
                     ->where('available', 1)
-                    ->select('title', 'apartment_description', 'rooms', 'beds', 'bathroom', 'square_mt', 'slug', 'img_path', 'id')
+                    ->select('title', 'apartment_description', 'rooms', 'beds', 'bathroom', 'square_mt', 'slug', 'img_path', 'id', 'sponsorship_id')
                     ->selectRaw(
                         '(6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance',
                         [$cord['latitude'], $cord['longitude'], $cord['latitude']]
