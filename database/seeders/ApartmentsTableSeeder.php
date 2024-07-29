6326f8d57687027ff362e55d3d2b3b5b47dd1b35
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-07-29 16:47:02 +0200

    fix: address from seeder-data

--- database/seeders/ApartmentsTableSeeder.php
+++ database/seeders/ApartmentsTableSeeder.php
@@ -5,18 +5,23 @@ namespace Database\Seeders;
 use App\Models\Apartment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Http\Controllers\Api\ApartmentController as ApiController;
+
 
 class ApartmentsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(ApiController $apiController): void
     {
         $apartmentsData = config('seeder-data');
         foreach ($apartmentsData as $index => $apartmentData) {
             $newApartment = new Apartment();
             $newApartment->fill($apartmentData);
+            $response = $apiController->getAddressFromCoordinates($newApartment->latitude, $newApartment->longitude);
+            $data = json_decode($response->getContent(), true);
+            $newApartment->address = $data['address'];
             $newApartment->img_path = 'apartment_images/p' . ($index + 1) . '.webp';
             $newApartment->save();
         }
